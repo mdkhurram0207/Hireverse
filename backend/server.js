c32ebeff9aae84056b6c25512dc3f6c7c0e39cc7
Mohammad Khurram <168674680+mdkhurram0207@users.noreply.github.com>
2025-08-15 01:13:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,11 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const path = require('path'); 
+const path = require('path');
 
 const clientRoutes = require('./routes/clientRoutes');
 const candidateRoutes = require('./routes/candidateRoutes');
 const jobRoutes = require('./routes/jobRoutes');
-
 
 dotenv.config();
 connectDB();
@@ -15,15 +14,14 @@ connectDB();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));// For resumes
 
-// Routes (api)
-app.use('/api/clients', clientRoutes);   //http://localhost:4000/api/clients/
-app.use('/api/candidates', candidateRoutes);   http://localhost:4000/api/candidates/
-app.use('/api/jobs', jobRoutes);   http://localhost:4000/api/jobs/
+// For resumes
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-
-
+// API Routes
+app.use('/api/clients', clientRoutes);    // http://localhost:4000/api/clients/
+app.use('/api/candidates', candidateRoutes); // http://localhost:4000/api/candidates/
+app.use('/api/jobs', jobRoutes);          // http://localhost:4000/api/jobs/
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
